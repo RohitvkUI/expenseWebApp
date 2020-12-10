a2f84bc736cdb5f3eb8ba468bad1903fc4e2be63
Rohit <rohitvk2724@gmail.com>
2020-12-10 15:05:15 -0800

    changed proj style from css to scss

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'expenseapp'`, () => {
+  it(`should have as title 'expense-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('expenseapp');
+    expect(app.title).toEqual('expense-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('expenseapp app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('expense-app app is running!');
   });
 });
